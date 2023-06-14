0817a6596afeebd510335a621a81b5627ad4f8d1
Dinesh Singh <=>
2023-06-14 19:25:05 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="social_gpt",
-    version="0.0.10",
+    version="0.0.11",
     author="dinesh1301",
     author_email="dinesh@topmate.io",
     description="Social gpt",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dinesh1301/social-gpt2",
+    url="https://github.com/topmate-io/social_gpt",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
